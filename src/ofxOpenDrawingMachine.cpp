4ea826ec5cc93ce4eb34def932c2da63f7105835
montoyamoraga <montoyamoraga@gmail.com>
2020-10-09 10:55:22 -0400

    add function for drawing circle and arc

--- src/ofxOpenDrawingMachine.cpp
+++ src/ofxOpenDrawingMachine.cpp
@@ -47,11 +47,26 @@ void ofxOpenDrawingMachine::instrumentMoveTo(float newX, float newY) {
     serial.writeBytes(&buf[0], sizeof(buf));
 }
 
-void ofxOpenDrawingMachine::instrumentDrawTo(float newX, float newY) {
+void ofxOpenDrawingMachine::instrumentLineTo(float newX, float newY) {
     std::string str = "G01 X " + std::to_string(newX) + " Y " + std::to_string(newY) +  " F " + std::to_string(feedRate) + "\n";
     unsigned char buf[str.length()];
     for(int i = 0; i < sizeof(buf); i++){
         buf[i] = str[i];
+    }
+    serial.writeBytes(&buf[0], sizeof(buf));
+}
+
+void ofxOpenDrawingMachine::instrumentArcTo(float newX, float newY, float arcRadius, bool isClockwise) {
+    std::string str = "";
+    if (isClockwise) {
+        str = "G02 X " + std::to_string(newX) + " Y " + std::to_string(newY) + " R " + std::to_string(arcRadius) + " F " + std::to_string(feedRate) + "\n";
+    }
+    else {
+        str = "G03 X " + std::to_string(newX) + " Y " + std::to_string(newY) + " R " + std::to_string(arcRadius) + " F " + std::to_string(feedRate) + "\n";
+    }
+    unsigned char buf[str.length()];
+    for(int i = 0; i < sizeof(buf); i++){
+            buf[i] = str[i];
     }
     serial.writeBytes(&buf[0], sizeof(buf));
 }
@@ -60,8 +75,23 @@ void ofxOpenDrawingMachine::drawLine(float startX, float startY, float endX, flo
     instrumentUp();
     instrumentMoveTo(startX, startY);
     instrumentDown();
-    instrumentDrawTo(endX, endY);
+    instrumentLineTo(endX, endY);
     instrumentUp();
+}
+
+void ofxOpenDrawingMachine::drawArc(float startX, float startY, float endX, float endY, float arcRadius, bool isClockwise) {
+    instrumentUp();
+    instrumentMoveTo(startX, startY);
+    instrumentDown();
+    instrumentArcTo(endX, endY, arcRadius, isClockwise);
+    instrumentUp();
+}
+
+void ofxOpenDrawingMachine::drawCircle(float posX, float posY, float radius) {
+    drawArc(posX - radius, posY         , posX         , posY + radius, radius, true);
+    drawArc(posX         , posY + radius, posX + radius, posY         , radius, true);
+    drawArc(posX + radius, posY         , posX         , posY - radius, radius, true);
+    drawArc(posX         , posY - radius, posX - radius, posY         , radius, true);
 }
 
 void ofxOpenDrawingMachine::keyboardControl(int key) {
--- src/ofxOpenDrawingMachine.h
+++ src/ofxOpenDrawingMachine.h
@@ -33,10 +33,17 @@ class ofxOpenDrawingMachine {
     ///\param newY // new y position
     void instrumentMoveTo(float newX, float newY);
 
-    ///\brief uses the instrument to draw from current position to new XY position
+    ///\brief uses the instrument to draw a line to new position
     ///\param newX // new x position
     ///\param newY // new y position
-    void instrumentDrawTo(float newX, float newY);
+    void instrumentLineTo(float newX, float newY);
+    
+    ///\brief uses the instrument to draw an arc to new position, using radius and direction
+    ///\param newX // new x position
+    ///\param newX // new y position
+    ///\param arcRadius // arc radius
+    ///\param isClockwise // arc direction, true for clockwise, false is counterclockwise
+    void instrumentArcTo(float newX, float newY, float arcRadius, bool isClockwise);
     
     ///\brief draws line between start point and end point
     ///\param startX // x position of starting point
@@ -44,6 +51,18 @@ class ofxOpenDrawingMachine {
     ///\param endX // x position of ending point
     ///\param endY // y position of ending point
     void drawLine(float startX, float startY, float endX, float endY);
+    
+    ///\brief draws arc between start point and end point
+    ///\param startX // x position of starting point
+    ///\param startY // y position of starting point
+    ///\param endX // x position of ending point
+    ///\param endY // y position of ending point
+    ///\param arcRadius // arc radius
+    ///\param isClockwise // arc direction, true for clockwise, false is counterclockwise
+    void drawArc(float startX, float startY, float endX, float endY, float arcRadius, bool isClockwise);
+    
+    ///\brief draws circle around a point, using radius
+    void drawCircle(float posX, float posY, float radius);
     
     ///\brief controls the machine with key presses
     void keyboardControl(int key);
